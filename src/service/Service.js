84b04a8edb59b13b701e1f7c3329dd910428c355
614259004 <614259004@webmail.npru.ac.th>
2021-10-15 10:54:36 +0700

    Change Host

--- src/service/Service.js
+++ src/service/Service.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 
-let url = "http://spzone.cyberlifecafe.com";
+let url = "http://spzoneonline.com";
 let url2 = "https://trackapi.thailandpost.co.th/post/api/v1/track";
 
 let axiosConfig = {
